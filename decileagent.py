e4b219a91d627e71ec682d4fc7926a8091da4b62
tpike3 <bugdaddy08@gmail.com>
2018-06-22 09:44:34 -0300

    Added additional wealth partition; added comments

--- decileagent.py
+++ decileagent.py
@@ -10,10 +10,15 @@ from mesa import Agent
 
 class DecileAgent(Agent):
     """ An agent with fixed initial wealth."""
-    def __init__(self, unique_id, model, wealth, income, decile):
+    def __init__(self, unique_id, model, wealth, wealth_2, income, decile):
         super().__init__(unique_id, model)
+        # initial wealth
         self.wealth = wealth
+        # second wealth parameter to allow for reinvestment
+        self.wealth_2 = wealth_2
+        # income attribute 
         self.income = income
+        # which decile (economic class) agent belongs too
         self.decile = decile
 
     def step(self):
--- inequalitymodel.py
+++ inequalitymodel.py
@@ -22,6 +22,7 @@ class InequalityModel(Model):
     
     def __init__(self):
         self.num_agents = 10000
+        self.num_decs = int(self.num_agents/10)
         # Create agents
         self.decile = self.create_deciles()
         self.schedule = RandomActivation(self)
@@ -29,9 +30,10 @@ class InequalityModel(Model):
             #get correct decile for agent population
             pos = i//1000 
             #print ((self.decile[pos][1]))
-            income = self.decile[pos][0][i%1000]
-            wealth=self.decile[pos][1][i%1000]
-            a = da.DecileAgent(i, self, wealth, income,pos)
+            income = self.decile[pos][0][i%self.num_decs]
+            wealth=self.decile[pos][1][i%self.num_decs]
+            wealth_2 = self.decile[pos][2][i%self.num_decs]
+            a = da.DecileAgent(i, self, wealth, wealth_2, income,pos)
             self.schedule.add(a)
 
         #self.running = True
@@ -43,12 +45,16 @@ class InequalityModel(Model):
         deciles = []
         income = 0
         wealth = 10
+        wealth_2 = 0
         for i in range(10):
-            array_i = list(np.random.uniform(income,income +1, 1000))
-            array_w  = list(np.random.uniform(wealth, wealth+10, 1000))
-            deciles.append([array_i, array_w])
+            array_i = list(np.random.uniform(income,income +1, self.num_decs))
+            array_w  = list(np.random.uniform(wealth, wealth+10, self.num_decs))
+            array_w2 = list(np.random.uniform(wealth_2, wealth_2+1, self.num_decs))
+            deciles.append([array_i, array_w, array_w2])
             income += 1
             wealth += 10
+            #does the second wealth parameter need to increase or is it common to all deciles? 
+            wealth += 1
         #print (len(deciles[0]))
         return deciles
 
